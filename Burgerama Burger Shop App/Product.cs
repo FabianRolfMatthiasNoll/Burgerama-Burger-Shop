7950e640062b966cb1eae56a8c8a06b709f5b596
fanoll <fabian.noll@karlstorz.com>
2022-10-07 15:07:05 +0200

    - implemented interfacing (WIP)

--- Burgerama Burger Shop App/Product.cs
+++ Burgerama Burger Shop App/Product.cs
@@ -5,8 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Burgerama_Burger_Shop_App
-{
-    internal class Product
+{   
+    enum Size
+    {
+        S = 0,
+        M = 1,
+        L = 2,
+        XL = 3,
+        XXL = 4
+    }
+    interface IDrink
+    {
+        void IsOnIce();
+    }
+    interface IMerchandise
+    {
+        void SetSize(int input);
+    }
+    internal class Product : IDrink, IMerchandise
     {
         public int id;
         public string category;
@@ -14,10 +30,7 @@ namespace Burgerama_Burger_Shop_App
         public string name;
         public double price;
         public int prepTime;
-
-        public string size; //(S,M,L,XL,XXL)
-        bool onIce;         //true = with Ice,,false = without Ice
-
+        public Size size;
 
         public Product(int prodId, string prodCategory, string prodName, double prodPrice, int prodPrepTime, int prodCategoryId)
         {
@@ -28,5 +41,31 @@ namespace Burgerama_Burger_Shop_App
             price = prodPrice;
             prepTime = prodPrepTime;
         }
+        
+        void IDrink.IsOnIce()
+        {
+            bool onIce;         //true = with Ice,,false = without Ice
+        }
+
+        void IMerchandise.SetSize(int input)
+        {
+            //ask robin what we can simplify here
+            if(input == 0)
+            {
+                size = Size.S;
+            } else if (input == 1)
+            {
+                size = Size.M;
+            } else if (input == 2)
+            {
+                size = Size.L;
+            } else if (input == 3)
+            {
+                size = Size.XL;
+            } else if (input == 4)
+            {
+                size = Size.XXL;
+            }
+        }                
     }
 }
