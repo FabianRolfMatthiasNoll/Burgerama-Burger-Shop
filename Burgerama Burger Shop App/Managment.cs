3899f7c068ebda3f76a679d16312d0d95b932198
fanoll <fabian.noll@karlstorz.com>
2022-10-10 16:27:00 +0200

    - added managment site - added time simulation - added order overview

---
+++ Burgerama Burger Shop App/Managment.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleTables;
+
+namespace Burgerama_Burger_Shop_App
+{
+    internal class Managment
+    {
+        public static void ManagerMenu()
+        {
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("                                             Welcome Back Manager");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("                                          Here are all existing Orders:             ");
+            var table = new ConsoleTable("Email of User", "Address", "Driver", "Prep. Time", "Ship. Time", "Status");
+            List<Driver> drivers = Driver.LoadCurrentDriverStates();
+            foreach(var driver in drivers)
+            {
+                foreach(var order in driver.orders)
+                {
+                    table.AddRow(order.customer.email,order.customer.street + " " + order.customer.postal + " " + order.customer.city, driver.name, order.prepTime, order.shipTime, order.state);
+                }
+            }
+            table.Write(Format.Alternative);
+
+            Console.WriteLine("\nManagment System Online");
+            Console.WriteLine("1) Move Time forward by 15min");
+            Console.WriteLine("2) Delete all Orders that are 'closed'");
+            Console.Write("What do you want to do?: ");
+
+            int menu = Convert.ToInt32(Console.ReadLine());
+
+            while (menu < 1 || menu > 2)
+            {
+                //If entered value isnt a integer an error occurs
+                Program.ClearCurrentConsoleLine();
+                Console.Write("Please enter a valid Option: ");
+                menu = Convert.ToInt32(Console.ReadLine());
+            }
+            if (menu == 1)
+            {
+                SpeedUpTime(drivers);
+                ManagerMenu();
+            }
+            else
+            {
+                EraseClosedOrders(drivers);
+                ManagerMenu();
+            }
+        }
+
+        static void SpeedUpTime(List<Driver> drivers)
+        {
+            foreach (var driver in drivers)
+            {
+                foreach (var order in driver.orders)
+                {
+                    Order.DecreaseTotalTime(order);
+                }
+            }
+            Driver.SaveCurrentDriverStates(drivers);
+            Console.Clear();
+        }
+
+        static void EraseClosedOrders(List<Driver> drivers)
+        {
+
+        }
+    }
+}
