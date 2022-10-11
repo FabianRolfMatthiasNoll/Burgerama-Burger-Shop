eb9a378bebe3e9d60a8ffecea63609d6a677ce77
fanoll <fabian.noll@karlstorz.com>
2022-10-11 13:05:33 +0200

    - added file validation - added possibility to add Drivers - added control of product IDs - added console input control

--- Burgerama Burger Shop App/src/Program.cs
+++ Burgerama Burger Shop App/src/Program.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.Reflection.Metadata;
-using System.Collections.Generic;
-using System.Xml;
-using System.Xml.Linq;
-using System.Xml.Serialization;
-using System.Security.Principal;
-using System.Text;
-using System.Collections;
-using Newtonsoft.Json;
-using Formatting = Newtonsoft.Json.Formatting;
+﻿using Burgerama_Burger_Shop_App.products;
 
 namespace Burgerama_Burger_Shop_App
 {
@@ -17,16 +6,16 @@ namespace Burgerama_Burger_Shop_App
     {
         public static void Main()
         {
+            //check if all necessary files exist
+            CheckFileDependencies();
+
+            //update drivers if driver_config.json has been changed
+            Driver.UpdateDrivers();
+
+            //check all product ids and throw an error if there are duplicate ones
+            Product.CheckProductID();
 
             Console.SetWindowSize(140, 30);
-
-            //Registration.RegistrationMenu();
-
-            //Login.LoginMenu();
-
-            //Ordering.OrderMenu("Test@email.com");
-
-            //End of Debugging Bypass
 
             Console.WriteLine("\n                            |\\ /| /|_/|\r\n                          |\\||-|\\||-/|/|\r\n                           \\\\|\\|//||///\r\n          _..----.._       |\\/\\||//||||\r\n        .'     o    '.     |||\\\\|/\\\\ ||\r\n       /   o       o  \\    | './\\_/.' |\r\n      |o        o     o|   |          |\r\n      /'-.._o     __.-'\\   |          |\r\n      \\      `````     /   |          |\r\n      |``--........--'`|    '.______.'\r\n       \\              /\r\n        `'----------'`\n");
             Console.WriteLine("Welcome to Burgerama Burger Shop");
@@ -35,22 +24,83 @@ namespace Burgerama_Burger_Shop_App
             Console.WriteLine("(2) Login at Burgerama");
             Console.WriteLine("");
             Console.Write("Please select an Option: ");
-            int menu = Convert.ToInt32(Console.ReadLine());
+            var menuInput = Console.ReadLine();
+            int menu;
+            while (true)
+            {
+                while (!int.TryParse(menuInput, out menu))
+                {
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    ClearCurrentConsoleLine();
+                    Console.Write("Please enter a number!:");
+                    menuInput = Console.ReadLine();
+                }
 
-            while(menu < 1 || menu > 2)
-            {
-                //If entered value isnt a integer an error occurs
-                ClearCurrentConsoleLine();
-                Console.Write("Please enter a valid Option: ");
-                menu = Convert.ToInt32(Console.ReadLine());
+                if (menu < 1 || menu > 2)
+                {
+                    //If entered value isnt a integer an error occurs
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    ClearCurrentConsoleLine();
+                    Console.Write("Please enter a valid Option: ");
+                    menuInput = Console.ReadLine();
+                }
+
+                if (menu == 1)
+                {
+                    Registration.RegistrationMenu();
+                } else if (menu == 2)
+                {
+                    Login.LoginMenu();
+                }
             }
-            if(menu == 1)
+        }
+
+        public static void CheckFileDependencies()
+        {
+            Console.WriteLine("Loading User Data...");
+            if (!File.Exists("src/data/user_data.xml"))
             {
-                Registration.RegistrationMenu();
-            } else
-            {
-                Login.LoginMenu();
+                Console.WriteLine("[Error] user data wasnt found!");
+                Console.ReadKey();
+                Environment.Exit(0);
             }
+            Thread.Sleep(1000);
+            Console.WriteLine("User Data loaded successfully");
+            Thread.Sleep(500);
+            Console.WriteLine("Loading Product Data...");
+            if (!File.Exists("src/data/product_data.json"))
+            {
+                Console.WriteLine("[Error] Product Data wasn`t found!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            Thread.Sleep(1000);
+            Console.WriteLine("Product Data loaded successfully");
+            Thread.Sleep(500);
+            Console.WriteLine("Loading Driver Configurations...");
+            if (!File.Exists("src/data/driver_config.json"))
+            {
+                Console.WriteLine("[Error] Driver Configuration wasnt found!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            Thread.Sleep(1000);
+            Console.WriteLine("Driver Configuration loaded successfully");
+            Thread.Sleep(500);
+            Console.WriteLine("Loading Driver Data...");
+            if (!File.Exists("src/data/driver_data.json"))
+            {
+                Console.WriteLine("[Error] Driver Data wasnt found!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            Thread.Sleep(1000);
+            Console.WriteLine("Driver Data loaded successfully");
+            Thread.Sleep(500);
+            Console.WriteLine("All Files are Ready. Press any key to start the application");
+            Console.ReadKey();
+            Console.Clear();
+
         }
 
         public static void ClearCurrentConsoleLine()
