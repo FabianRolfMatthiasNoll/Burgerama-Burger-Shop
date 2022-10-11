c642339a2b79041be0315d81f0ae5f70d898592f
fanoll <fabian.noll@karlstorz.com>
2022-10-11 15:27:55 +0200

    - added for registration input validation

---
+++ Burgerama Burger Shop App/src/City.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burgerama_Burger_Shop_App.src
+{
+    internal class City
+    {
+        public string city;
+        public string country;
+        public string admin_name;
+    }
+}
