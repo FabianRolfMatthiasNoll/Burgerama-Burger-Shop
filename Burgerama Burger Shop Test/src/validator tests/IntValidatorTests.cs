a3842a67fc0b98c0118fc072c2d0adba8b271fdc
fanoll <fabian.noll@karlstorz.com>
2022-10-14 09:57:04 +0200

    - added tests for new valdiator methods

--- Burgerama Burger Shop Test/src/validator tests/IntValidatorTests.cs
+++ Burgerama Burger Shop Test/src/validator tests/IntValidatorTests.cs
@@ -15,6 +15,7 @@ namespace Burgerama_Burger_Shop_Test.src.validator_tests
         [InlineData(3)]
         public void IsIntValidPass(int input)
         {
+            //submethod isInputinBound is tested within here
             IntValidator intValidator = new IntValidator(1, 3);
             bool output;
 
@@ -63,6 +64,91 @@ namespace Burgerama_Burger_Shop_Test.src.validator_tests
             bool output;
 
             output = intValidator.IsInputValid(input);
+
+            Assert.False(output);
+        }
+
+        [Theory]
+        [InlineData("3")]
+        [InlineData("-10")]
+        [InlineData("0")]
+        public void IsStringAnIntPass(string input)
+        {
+            IntValidator intValidator = new IntValidator(0, 0);
+            bool output;
+
+            output = intValidator.IsInputInt(input);
+
+            Assert.True(output);
+        }
+
+        [Theory]
+        [InlineData("Hallo")]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("A1")]
+        public void IsStringAnIntFail(string input)
+        {
+            IntValidator intValidator = new IntValidator(0, 0);
+            bool output;
+
+            output = intValidator.IsInputInt(input);
+
+            Assert.False(output);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(5)]
+        [InlineData(3.7)]
+        public void IsIntPositivPass(int input)
+        {
+            IntValidator intValidator = new IntValidator(0, 0);
+            bool output;
+
+            output = intValidator.IsIntPositiv(input);
+
+            Assert.True(output);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-5)]
+        [InlineData(-3.4)]
+        public void IsIntPositivFail(int input)
+        {
+            IntValidator intValidator = new IntValidator(0, 0);
+            bool output;
+
+            output = intValidator.IsIntPositiv(input);
+
+            Assert.False(output);
+        }
+
+        [Theory]
+        [InlineData("0")]
+        [InlineData("5")]
+        public void IsStringPositivPass(string input)
+        {
+            IntValidator intValidator = new IntValidator(0, 0);
+            bool output;
+
+            output = intValidator.IsIntPositiv(input);
+
+            Assert.True(output);
+        }
+
+        [Theory]
+        [InlineData("-1")]
+        [InlineData("-5")]
+        [InlineData("-3.4")]
+        [InlineData("3.7")]
+        public void IsStringPositivFail(string input)
+        {
+            IntValidator intValidator = new IntValidator(0, 0);
+            bool output;
+
+            output = intValidator.IsIntPositiv(input);
 
             Assert.False(output);
         }
