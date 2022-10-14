7813656da848d80fde843316603410794ec26de2
fanoll <fabian.noll@karlstorz.com>
2022-10-14 13:06:58 +0200

    - added a BoolValidator

---
+++ Burgerama Burger Shop App/src/validators/BoolValidator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burgerama_Burger_Shop_App.src.validators
+{
+    public class BoolValidator
+    {
+        StringValidator stringValidator;
+
+        public BoolValidator()
+        {
+            stringValidator = new StringValidator();
+        }
+
+        public bool IsStringValidBool(string input)
+        {
+            input = input.ToLower();
+            if (stringValidator.IsStringEmpty(input))
+            {
+                return false;
+            }
+
+            if (input == "true" || input == "false")
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
