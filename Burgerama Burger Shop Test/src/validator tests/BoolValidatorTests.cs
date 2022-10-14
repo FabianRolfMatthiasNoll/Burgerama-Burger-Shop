b91a34a6393322011fc0649f4ea6d24d414f355f
fanoll <fabian.noll@karlstorz.com>
2022-10-14 14:28:22 +0200

    - added unit Tests for Bool Validator

--- Burgerama Burger Shop Test/src/validator tests/BoolValidatorTests.cs
+++ Burgerama Burger Shop Test/src/validator tests/BoolValidatorTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Burgerama_Burger_Shop_App.src.validators;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,38 @@ using System.Threading.Tasks;
 
 namespace Burgerama_Burger_Shop_Test.src.validator_tests
 {
-    internal class BoolValidatorTests
+    public class BoolValidatorTests
     {
+        [Theory]
+        [InlineData("true")]
+        [InlineData("false")]
+        [InlineData("False")]
+        [InlineData("TRUE")]
+        [InlineData("fAlsE")]
+        public void IsStringAValidBoolPass(string input)
+        {
+            BoolValidator boolValidator = new BoolValidator();
+            bool output;
+
+            output = boolValidator.IsStringValidBool(input);
+
+            Assert.True(output);
+        }
+
+        [Theory]
+        [InlineData("10")]
+        [InlineData("-10")]
+        [InlineData("Hello")]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void IsStringAValidBoolFail(string input)
+        {
+            BoolValidator boolValidator = new BoolValidator();
+            bool output;
+
+            output = boolValidator.IsStringValidBool(input);
+
+            Assert.False(output);
+        }
     }
 }
