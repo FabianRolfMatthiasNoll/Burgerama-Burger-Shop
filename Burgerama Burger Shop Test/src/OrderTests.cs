22a09575a34bef8872bd29cc70ddb7ecf9e2f5c9
fanoll <fabian.noll@karlstorz.com>
2022-10-17 10:53:21 +0200

    - adjusted and added unit tests

--- Burgerama Burger Shop Test/src/OrderTests.cs
+++ Burgerama Burger Shop Test/src/OrderTests.cs
@@ -11,7 +11,7 @@ namespace Burgerama_Burger_Shop_Test.src
     public class OrderTests
     {
         [Fact]
-        public void CalculateOrderAttributes()
+        public void CalculateOrderAttributesFoodAndAvailableDriver()
         {
             Order order = new Order("src/test data/", "driver_data_test.json");
             User user = new User();
@@ -28,7 +28,64 @@ namespace Burgerama_Burger_Shop_Test.src
             Assert.Equal(35, order.totalTime);
             Assert.Equal(user.email, order.customer.email);
         }
+        
+        [Fact]
+        public void CalculateOrderAttributesFoodAndUnavailableDriver()
+        {
+            Order order = new Order("src/test data/", "driver_data_test_no_free.json");
+            User user = new User();
+            user.email = "TestEmail";
+            order.boughtProducts.Add(new Product(1, "Food", "Burger", 4, 10, 1));
+            order.boughtProducts.Add(new Product(1, "Food", "BurgerXL", 6, 15, 1));
 
+            order.FillInformationInOrder(user);
+
+            Assert.Equal(15, order.prepTime);
+            Assert.Equal(State.Preperation, order.state);
+            Assert.Equal(10, order.totalSum);
+            Assert.Equal(35, order.shipTime);
+            Assert.Equal(50, order.totalTime);
+            Assert.Equal(user.email, order.customer.email);
+        }
+
+        [Fact]
+        public void CalculateOrderAttributesMerchandiseAndAvailableDriver()
+        {
+            Order order = new Order("src/test data/", "driver_data_test.json");
+            User user = new User();
+            user.email = "TestEmail";
+            order.boughtProducts.Add(new Product(1, "Merchandise", "Burger", 4, 0, 1));
+            order.boughtProducts.Add(new Product(1, "Merchandise", "BurgerXL", 6, 0, 1));
+
+            order.FillInformationInOrder(user);
+
+            Assert.Equal(0, order.prepTime);
+            Assert.Equal(State.Delivery, order.state);
+            Assert.Equal(10, order.totalSum);
+            Assert.Equal(20, order.shipTime);
+            Assert.Equal(20, order.totalTime);
+            Assert.Equal(user.email, order.customer.email);
+        }
+
+        [Fact]
+        public void CalculateOrderAttributesMerchandiseAndUnavailableDriver()
+        {
+            Order order = new Order("src/test data/", "driver_data_test_no_free.json");
+            User user = new User();
+            user.email = "TestEmail";
+            order.boughtProducts.Add(new Product(1, "Merchandise", "Burger", 4, 0, 1));
+            order.boughtProducts.Add(new Product(1, "Merchandise", "BurgerXL", 6, 0, 1));
+
+            order.FillInformationInOrder(user);
+
+            Assert.Equal(0, order.prepTime);
+            Assert.Equal(State.Delivery, order.state);
+            Assert.Equal(10, order.totalSum);
+            Assert.Equal(35, order.shipTime);
+            Assert.Equal(35, order.totalTime);
+            Assert.Equal(user.email, order.customer.email);
+        }
+        
         //decrese total time is tested in managmantHandler but still to be added
     }
 }
