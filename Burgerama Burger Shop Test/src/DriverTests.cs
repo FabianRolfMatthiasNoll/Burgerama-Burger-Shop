1994469f985668230dd5499ef80a12de69f74ca5
fanoll <fabian.noll@karlstorz.com>
2022-10-17 08:28:54 +0200

    - added unit tests for driver class

---
+++ Burgerama Burger Shop Test/src/DriverTests.cs
@@ -0,0 +1,37 @@
+﻿using Burgerama_Burger_Shop_App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burgerama_Burger_Shop_Test.src
+{
+    public class DriverTests
+    {
+        [Fact]
+        public void IsDriverFreePass()
+        {
+            Driver driver = new Driver("Rosalin", 2);
+            driver.orders.Add(null);
+            bool output;
+
+            output = driver.IsDriverFree();
+
+            Assert.True(output);
+        }
+
+        [Fact]
+        public void IsDriverFreeFail()
+        {
+            Driver driver = new Driver("Rosalin", 2);
+            driver.orders.Add(null);
+            driver.orders.Add(null);
+            bool output;
+
+            output = driver.IsDriverFree();
+
+            Assert.False(output);
+        }
+    }
+}
