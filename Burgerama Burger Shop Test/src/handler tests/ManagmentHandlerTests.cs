3c1889640c724cadd52334a295572a1f72c23feb
fanoll <fabian.noll@karlstorz.com>
2022-10-17 10:52:30 +0200

    - added unit tests

--- Burgerama Burger Shop Test/src/handler tests/ManagmentHandlerTests.cs
+++ Burgerama Burger Shop Test/src/handler tests/ManagmentHandlerTests.cs
@@ -12,7 +12,7 @@ namespace Burgerama_Burger_Shop_Test.src.handler_tests
         [Fact]
         public void MoveTimeForward()
         {
-            ManagmentHandler managmentHandler = new ManagmentHandler("src/test data/", "driver_data_test.json");
+            ManagmentHandler managmentHandler = new ManagmentHandler("src/test data/", "driver_data_test_time1.json");
             managmentHandler.LoadDriverData();
 
             managmentHandler.FastForwardTime();
@@ -21,6 +21,34 @@ namespace Burgerama_Burger_Shop_Test.src.handler_tests
             Assert.Equal(15, managmentHandler.drivers[0].orders[0].shipTime);
             Assert.Equal(15, managmentHandler.drivers[0].orders[0].totalTime);
             Assert.Equal(Burgerama_Burger_Shop_App.State.Delivery, managmentHandler.drivers[0].orders[0].state);
+        }
+
+        [Fact]
+        public void MoveTimeForwardTwice()
+        {
+            ManagmentHandler managmentHandler = new ManagmentHandler("src/test data/", "driver_data_test_time2.json");
+            managmentHandler.LoadDriverData();
+
+            managmentHandler.FastForwardTime();
+
+            Assert.Equal(0, managmentHandler.drivers[0].orders[0].prepTime);
+            Assert.Equal(0, managmentHandler.drivers[0].orders[0].shipTime);
+            Assert.Equal(0, managmentHandler.drivers[0].orders[0].totalTime);
+            Assert.Equal(Burgerama_Burger_Shop_App.State.Closed, managmentHandler.drivers[0].orders[0].state);
+        }
+
+        [Fact]
+        public void MoveTimeForwardThrice()
+        {
+            ManagmentHandler managmentHandler = new ManagmentHandler("src/test data/", "driver_data_test_time3.json");
+            managmentHandler.LoadDriverData();
+
+            managmentHandler.FastForwardTime();
+
+            Assert.Equal(0, managmentHandler.drivers[0].orders[0].prepTime);
+            Assert.Equal(0, managmentHandler.drivers[0].orders[0].shipTime);
+            Assert.Equal(0, managmentHandler.drivers[0].orders[0].totalTime);
+            Assert.Equal(Burgerama_Burger_Shop_App.State.Closed, managmentHandler.drivers[0].orders[0].state);
         }
 
         [Fact]
