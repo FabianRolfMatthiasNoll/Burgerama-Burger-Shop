1777002e1d486b3ee54f284e4ecf13a791817ef7
fanoll <fabian.noll@karlstorz.com>
2022-10-24 08:34:22 +0200

    added interface for new mood functionality

---
+++ Burgerama Burger Shop App/src/interfaces/IMood.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burgerama_Burger_Shop_App.src.interfaces
+{
+    public interface IMood
+    {
+        public string Mood { get; set; }
+
+        public void CalculateMood();
+
+        public void CalculateMoodAfterTime();
+    }
+}
