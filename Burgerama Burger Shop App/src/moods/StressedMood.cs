121bf4fd26f9f3cb49ac91152567522eda5cf771
fanoll <fabian.noll@karlstorz.com>
2022-10-24 09:23:52 +0200

    added classes for different moods
    added mood classes that adapt the IMood interface.

---
+++ Burgerama Burger Shop App/src/moods/StressedMood.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Burgerama_Burger_Shop_App.src.interfaces;
+
+namespace Burgerama_Burger_Shop_App.src.moods
+{
+    public class StressedMood : IMood
+    {
+        public int DeliveryTimeFactor { get; }
+        public void SwitchToNextMood(int capacity, int openOrders)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SwitchToNextMoodTimeCycle(int capacity, int openOrders)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
